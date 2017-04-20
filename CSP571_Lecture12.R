38a7857bf7f2a5e455ea4b5184ff70832639ba47
royxss <findroy@outlook.in>
2017-04-20 12:46:29 -0500

    Update CSP571_Lecture12.R

--- CSP571_Lecture12.R
+++ CSP571_Lecture12.R
@@ -3,7 +3,7 @@
 
 # Let's revisit the Titanic data set
 # Download the Titanic data set from Kaggle: https://www.kaggle.com/c/titanic/data
-data <- read.csv('/Users/adam.mcelhinney/Downloads/Titanic/train.csv'
+data <- read.csv('C:\\Users\\SROY\\Documents\\CodeBase\\Datasets\\Titanic\\train.csv'
                  , header= TRUE
                  ,na.strings=c(""))
 
